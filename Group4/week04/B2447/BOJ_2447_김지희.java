34d81d5875ff778172e967cb68e6fc1c9735254d
jihee <51085309+4priltwntsx@users.noreply.github.com>
2022-08-20 23:37:37 +0900

    [UPDATE] 2447 김지희 수정

--- Group4/week04/B2447/BOJ_2447_김지희.java
+++ Group4/week04/B2447/BOJ_2447_김지희.java
@@ -3,29 +3,48 @@ package week04;
 import java.util.Scanner;
 //별찍기
 public class BOJ_2447_김지희 {
-
+	static char[][] stars;
 	public static void main(String[] args) {
 		Scanner sc =new Scanner(System.in);
 		
 		int N = sc.nextInt();
+		stars = new char[N][N];
+		StringBuilder sb = new StringBuilder();
+		
+		print_stars(N, 0, 0);
 		
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<N; j++) {
-				print_stars(N, i, j);
+				if(stars[i][j]!='*') sb.append(' ');
+				else {
+					sb.append(stars[i][j]);	
+				}
 			}
-			System.out.println();
+			sb.append("\n");
 		}
+		System.out.println(sb);
 	}
 
-	private static void print_stars(int n, int i, int j) {
-		//n은 3^k, (x, y) = (0,0);
-		if((i/n)%3 == 1 && (j/n)%3 == 1) {
-			System.out.print(" ");
+	private static void print_stars(int n, int x, int y) {
+		if(n==1) {
+			stars[x][y] = '*';
+			return;
 		}
-		else {
-			if(n/3==0) System.out.print("*");
-			else print_stars(n/3, i, j);
-		}
+		
+		int n3 = n/3;
+		int n32 = 2*n3;
+		
+		print_stars(n3, x, y);
+		print_stars(n3, x+n3, y);
+		print_stars(n3, x+n32, y);
+		
+		print_stars(n3, x, y+n3);
+		
+		print_stars(n3, x+n32, y+n3);
+		
+		print_stars(n3, x, y+n32);
+		print_stars(n3, x+n3, y+n32);
+		print_stars(n3, x+n32, y+n32);
 		
 	}
 	
