40727cbec6c8dcec3e983848a1ef3b525fec5ea4
임경찬 <rudcks5562@naver.com>
2022-08-22 22:13:50 +0900

    [UPDATE]: BOJ_17608 update

--- Group4/week04/B17608/BOJ_17608_임경찬(틀린버전).java
+++ Group4/week04/B17608/BOJ_17608_임경찬(틀린버전).java
@@ -1,3 +1,4 @@
+package D0822;
 
 import java.util.Scanner;
 import java.util.Stack;
@@ -12,46 +13,60 @@ public class Main {
 
 		Stack<Integer> stack = new Stack<>();
 
+		int max = 0;
+		int max_idx = 0;
+
 		for (int a = 0; a < n; a++) {
 
 			int s = sc.nextInt();
+
 			stack.add(s);
 
-		}
-        
-		int max_stack = stack.get(0);
-		int max_idx = 0;
-		int cur = 0;
-        
-		for (int a = 1; a < n - 1; a++) {
-			cur++;
+			if (max != Math.max(s, max)) {
 
-			if (stack.get(cur) >= max_stack) {// max보다 큰 경우 교체
-
-				max_stack = stack.get(cur);
-				max_idx= cur;
-
-				for (int as = max_idx; as < cur; as++) {
-					stack.remove(as);
-					cur--;
-
-				}
-
-			} else if (stack.get(cur) < max_stack) {// max보다 작은 경우
-
-				while (stack.get(cur) <= stack.get(cur + 1)) {
-					stack.remove(cur);
-					cur--;
-					if(cur==max_idx) {
-						break;
-					}
-				}
+				max = s;
+				max_idx = a;
 
 			}
 
 		}
 
+		for (int s = 0; s < max_idx; s++) {
+
+			stack.remove(0);
+		}
+		int temp = stack.size();
+
+		int cur = 1;
+
+		for (int s = 0; s < temp - 2; s++) {
+			
+			
+		//	System.out.println(cur);
+			while ((stack.get(cur) <= stack.get(cur + 1))) {
+			//	System.out.println(stack.get(cur) + " " + stack.get(cur + 1));
+				if (stack.get(cur) <= stack.get(cur + 1)) {
+
+					stack.remove(cur);
+					cur--;
+					
+					if (s == temp - 1  ) {
+						break;
+
+					}
+				}
+
+
+			}
+
+			cur++;
+
+			System.out.println(stack.toString());
+
+		}
+
 		System.out.println(stack.size());
+
 		return;
 
 	}
